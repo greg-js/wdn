1addd4af34db2202674e83809fcc42acf60a8300
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2016-06-21 00:18:54 +0100

    add exec feature for executing arbitrary commands on warppoints

--- bin/wdn.sh
+++ bin/wdn.sh
@@ -3,14 +3,25 @@
 # get script dir
 WD=`dirname $0`
 
-# detect keywords : help, version, list, add, remove
+# get warp dir function
+function getdir () {
+  local warp="$(node ${WD}/../lib/warp.js $1)"
+  if [[ $warp == "undefined" ]]; then
+    echo $arg is not a valid warp point
+    exit 1
+  fi
+  echo $warp
+}
+
+# detect keywords : help, version, list, add, remove, clear
 if [[ $1 =~ ^\-?\-?help$\|^\-?\-?h$\|^\-?\-?version$\|^\-?\-?v$\|^\-?\-?list$\|^\-?\-?ls$\|^\-?\-?add$\|^\-?\-?a$\|^\-?\-?remove$\|^\-?\-?rm$\|^\-?\-?clear$ ]]; then
   node ${WD}/cli.js $@
+# execute commands
+elif [[ $1 =~ ^\-?\-?exec$\|^\-?\-?e$ ]]; then
+  DIR=$(getdir $2)
+  echo "${@:3} $DIR" | $SHELL -
+# warp in all other cases
 else
-  DIR="$(node ${WD}/../lib/warp.js $@)"
-  if [[ $DIR == "undefined" ]]; then
-    echo $1 is not a valid warp point
-  else
-    cd $DIR
-  fi
+  DIR=$(getdir $1)
+  cd $DIR
 fi
