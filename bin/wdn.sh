0c8799a3638e50fe631079175403c52f9029dde7
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2016-06-21 03:04:02 +0100

    fixed dirname and regex issues in bash

--- bin/wdn.sh
+++ bin/wdn.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # get script dir
-WD=`dirname $0`
+if [[ $SHELL =~ zsh$ ]]; then
+  WD=$( cd "$(dirname "$0")" ; pwd -P )
+elif [[ $SHELL =~ bash$ ]]; then
+  WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
 
 # get warp dir function
 function getdir () {
@@ -13,11 +17,12 @@ function getdir () {
   echo $warp
 }
 
-# detect keywords : help, version, list, add, remove, clear
-if [[ $1 =~ ^\-?\-?help$\|^\-?\-?h$\|^\-?\-?version$\|^\-?\-?v$\|^\-?\-?list$\|^\-?\-?ls$\|^\-?\-?add$\|^\-?\-?a$\|^\-?\-?remove$\|^\-?\-?rm$\|^\-?\-?clear$ ]]; then
+# ridiculous mess for detecting keywords in both bash and zsh
+# tested keywords: help, h, version, v, list, ls, add, a, remove, rm, clear
+if [[ $1 =~ ^\-?\-?help$ || $1 =~ ^\-?\-?h$ || $1 =~ ^\-?\-?version$ || $1 =~ ^\-?\-?v$ || $1 =~ ^\-?\-?list$ || $1 =~ ^\-?\-?ls$ || $1 =~ ^\-?\-?add$ || $1 =~ ^\-?\-?a$ || $1 =~ ^\-?\-?remove$ || $1 =~ ^\-?\-?rm$ || $1 =~ ^\-?\-?clear$ ]]; then
   node ${WD}/cli.js $@
 # execute commands
-elif [[ $1 =~ ^\-?\-?exec$\|^\-?\-?e$ ]]; then
+elif [[ $1 =~ ^\-?\-?exec$ || $1 =~ ^\-?\-?e$ ]]; then
   DIR=$(getdir $2)
   echo "${@:3} $DIR" | $SHELL -
 # warp in all other cases
@@ -25,3 +30,4 @@ else
   DIR=$(getdir $1)
   cd $DIR
 fi
+i
