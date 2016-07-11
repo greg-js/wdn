4571342558b6086cf3ab450e008fea1e4cb229c4
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2016-07-11 15:32:35 +0100

    added warp tests

---
+++ test/warp.js
@@ -0,0 +1,69 @@
+/* eslint-env mocha */
+var expect = require('chai').expect;
+var rm = require('fs').unlink;
+var rmdir = require('fs').rmdir;
+var path = require('path');
+
+var customConfig = './test/fixtures/custom';
+
+var warp = require('../lib/warp');
+var store = require('../lib/store');
+
+var local = 'local';
+var remote = 'remote';
+
+// warping works by console.logging a path or message
+// which then gets read by a bash script (`./bin/wdn.sh`)
+describe('warp', function() {
+
+  before(function() {
+    store(local, customConfig).set('wdn', process.cwd());
+    store(local, customConfig).set('lib', path.resolve('lib'));
+    store(local, customConfig).set('test', path.resolve('test'));
+    store(local, customConfig).set('foo', '/not/a/real/path');
+
+    store(remote, customConfig).set('Jerry', 'Seinfeld');
+    store(remote, customConfig).set('Elaine', 'Benes');
+    store(remote, customConfig).set('George', 'Costanza');
+    store(remote, customConfig).set('Cosmo', 'Kramer');
+  });
+
+  it('should log an error message and return false if no point is given', function() {
+    expect(warp(local)).to.be.false;
+  });
+
+  it('should log null if the point doesn\'t exist in the store', function() {
+    expect(warp(local, 'not a point', customConfig)).to.be.a('null');
+    expect(warp(remote, 'not a point', customConfig)).to.be.a('null');
+  });
+
+  it('should log the path for an existing local point', function() {
+    expect(warp(local, 'lib', customConfig)).to.equal(path.resolve('lib'));
+    expect(warp(local, 'test', customConfig)).to.equal(path.resolve('test'));
+  });
+
+  it('should log relative paths for existing local points', function() {
+    expect(warp(local, 'wdn/lib', customConfig)).to.equal(path.resolve('lib'));
+    expect(warp(local, 'wdn\\test', customConfig)).to.equal(path.resolve('test'));
+  });
+
+  it('should log inaccessible for an inaccessible local point path', function() {
+    expect(warp(local, 'foo', customConfig)).to.equal('inaccessible');
+    expect(warp(local, 'wdn/foo/bar', customConfig)).to.equal('inaccessible');
+  });
+
+  it('should log a remote destination without validating the path', function() {
+    expect(warp(remote, 'Jerry', customConfig)).to.equal('Seinfeld');
+    expect(warp(remote, 'Elaine/George/Kramer', customConfig)).to.equal('Benes/George/Kramer');
+  });
+
+  after(function(done) {
+    rm(path.resolve(customConfig, local), function() {
+      rm(path.resolve(customConfig, remote), function() {
+        rmdir(path.resolve(customConfig), function() {
+          done();
+        });
+      });
+    });
+  });
+});
